daaafaafce3d2cc1d2cb4a40db57250ccd6cdaf3
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-09-26 01:55:13 -0300

    connecting to couchify cluster using java sdk instead of using psring data couchbase

--- src/main/java/com/github/pedroluiznogueira/couchify/CouchifyApplication.java
+++ src/main/java/com/github/pedroluiznogueira/couchify/CouchifyApplication.java
@@ -1,13 +1,25 @@
 package com.github.pedroluiznogueira.couchify;
 
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.Collection;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CouchifyApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... args) {
 		SpringApplication.run(CouchifyApplication.class, args);
-	}
 
+		// connect to couchbase cluster
+		final Cluster cluster = Cluster.connect("localhost", "cbuser", "password");
+
+		// obtain bucket
+		final Bucket bucket = cluster.bucket("couchmusic2");
+
+		// obtain collection
+		final Collection collection = bucket.defaultCollection();
+	}
 }
--- src/main/java/com/github/pedroluiznogueira/couchify/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.pedroluiznogueira.couchify.repository;
-
-import com.github.pedroluiznogueira.couchify.entity.TestEntity;
-import org.springframework.data.couchbase.repository.CouchbaseRepository;
-
-public interface TestRepository extends CouchbaseRepository<TestEntity, String> {
-}
